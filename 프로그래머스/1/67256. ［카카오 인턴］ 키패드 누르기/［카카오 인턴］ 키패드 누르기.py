8fd13441ec8c095df16ad981bf00e00859e2d679
LSH <65267061+LSH-1082@users.noreply.github.com>
2024-10-24 18:51:35 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.42 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,46 @@
+def solution(numbers, hand):
+    keypad = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9],
+              ["*", 0, "#"]]
+    answer = ''
+    left_thumb = "*"
+    right_thumb = "#"
+    for num in numbers:
+        if num in [1, 4, 7]:
+            answer += "L"
+            left_thumb = num
+        if num in [3, 6, 9]:
+            answer += "R"
+            right_thumb = num
+        if num in [2, 5, 8, 0]:
+            for i in range(len(keypad)):
+                if left_thumb in keypad[i]:
+                    l_loc = i
+                if right_thumb in keypad[i]:
+                    r_loc = i
+                if num in keypad[i]:
+                    n_loc = i
+            l_dis = abs(l_loc - n_loc) if left_thumb in [2, 5, 8, 0] else abs(l_loc - n_loc) + 1
+            r_dis = abs(r_loc - n_loc) if right_thumb in [2, 5, 8, 0] else abs(r_loc - n_loc) + 1
+            if l_dis == r_dis:
+                if hand == "right":
+                    answer += "R"
+                    right_thumb = num
+                else:
+                    answer += "L"
+                    left_thumb = num
+            elif l_dis > r_dis:
+                answer += "R"
+                right_thumb = num
+            elif l_dis < r_dis:
+                answer += "L"
+                left_thumb = num
+
+
+    return answer
+
+numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+hand = "right"
+
+print(solution(numbers, hand))
