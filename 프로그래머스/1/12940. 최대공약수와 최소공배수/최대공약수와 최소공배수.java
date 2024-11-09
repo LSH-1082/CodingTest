5b17d16d6504fd9d146e505762d338db73c6d8e7
LSH <65267061+LSH-1082@users.noreply.github.com>
2024-11-09 19:03:13 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -3,13 +3,13 @@ class Solution {
         int[] answer = new int[2];
         int max = Math.max(n, m);
         int min = Math.min(n, m);
-        answer[0] = mod(max, min);
-        answer[1] = answer[0] == 1 ? max * min : answer[0] * (max/answer[0]) * (min/answer[0]);
+        answer[0] = gcd(max, min);
+        answer[1] = answer[0] == 1 ? max * min : max * min /answer[0];
         return answer;
     }
 
-    public int mod(int max, int min) {
+    public int gcd(int max, int min) {
         int result = max % min;
-        return result == 0 ? min : mod(min, result);
+        return result == 0 ? min : gcd(min, result);
     }
 }
