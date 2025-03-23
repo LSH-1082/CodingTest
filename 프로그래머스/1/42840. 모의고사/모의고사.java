8e23308208ed1c39e7c690220e69df0189e3e8cd
LSH <65267061+LSH-1082@users.noreply.github.com>
2025-03-23 15:30:16 +0900

    [level 1] Title: 모의고사, Time: 3.15 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,22 +1,26 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 
 class Solution {
-    public int[] solution(int[] answers) {
+    public static int[] solution(int[] answers) {
         ArrayList<Integer> list = new ArrayList<>();
         int[] first = {1, 2, 3, 4, 5};
         int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] scores = new int[4]; // 0번 인덱스는 사용하지 않음
+
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == first[i % first.length]) map.put(1, map.getOrDefault(1, 0) + 1);
-            if (answers[i] == second[i % second.length]) map.put(2, map.getOrDefault(2, 0) + 1);
-            if (answers[i] == third[i % third.length]) map.put(3, map.getOrDefault(3, 0) + 1);
+            if (answers[i] == first[i % first.length]) scores[1]++;
+            if (answers[i] == second[i % second.length]) scores[2]++;
+            if (answers[i] == third[i % third.length]) scores[3]++;
         }
-        int max = Math.max(map.getOrDefault(1, 0), Math.max(map.getOrDefault(2, 0), map.getOrDefault(3, 0)));
-        for (int i = 0; i <= 3; i++) {
-            if (map.getOrDefault(i, 0) ==  max) list.add(i);
+
+        int max = Math.max(scores[1], Math.max(scores[2], scores[3]));
+
+        for (int i = 1; i <= 3; i++) {
+            if (scores[i] == max) list.add(i);
         }
+
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
