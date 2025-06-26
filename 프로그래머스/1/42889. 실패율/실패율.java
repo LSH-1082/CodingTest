5d65ac8decacee28fdb32083714daa5ce5c45387
LSH <65267061+LSH-1082@users.noreply.github.com>
2025-06-26 15:14:17 +0900

    [level 1] Title: 실패율, Time: 352.30 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int notClearPlayer = 0;
+        int clearPlayer = 0;
+        int notReachPlayer = 0;
+        HashMap<Integer, Float> map = new HashMap<>();
+        for (int i = 1; i < N + 1; i++) {
+            for (int num : stages) {
+                if(i == num) notClearPlayer++;
+                else if(i < num) clearPlayer++;
+                else notReachPlayer++;
+            }
+            float failRate = (float) notClearPlayer / (clearPlayer + notClearPlayer);
+            map.put(i, notReachPlayer == stages.length ? 0 : failRate);
+            notReachPlayer = 0;
+            notClearPlayer = 0;
+            clearPlayer = 0;
+        }
+
+        List<Map.Entry<Integer, Float>> entryList = new ArrayList<>(map.entrySet());
+
+        entryList.sort((e1, e2) -> {
+            int cmp = e2.getValue().compareTo(e1.getValue());
+            if (cmp == 0) {
+                return e1.getKey().compareTo(e2.getKey());
+            }
+            return cmp;
+        });
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Float> entry : entryList) {
+            list.add(entry.getKey());
+        }
+
+        return Arrays.stream(list.toArray(new Integer[0])).mapToInt(Integer::intValue).toArray();
+    }
+}
