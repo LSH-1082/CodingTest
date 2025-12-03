f4183d9e9b267018364be75913d0288500a81d3e
LSH <65267061+LSH-1082@users.noreply.github.com>
2025-12-03 23:15:33 +0900

    [Bronze V] Title: 이상한 기호, Time: 116 ms, Memory: 14808 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        bw.write((nums[0] * nums[0]) - (nums[1] * nums[1]) + "");
+        double[] nums = Arrays.stream(br.readLine().split(" ")).mapToDouble(Double::valueOf).toArray();
+        bw.write(String.format("%.0f",(nums[0] * nums[0]) - (nums[1] * nums[1])));
         bw.flush();
     }
 }
