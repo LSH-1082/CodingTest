2db4b74946f66d186479cdb339b672c5f95c0493
LSH <65267061+LSH-1082@users.noreply.github.com>
2026-01-25 15:01:48 +0900

    [Silver IV] Title: 듣보잡, Time: 304 ms, Memory: 26524 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<String> s = new HashSet<>();
+        LinkedList<String> ls = new LinkedList<>();
+
+        int notHeard = Integer.parseInt(st.nextToken());
+        int notLooked = Integer.parseInt(st.nextToken());
+        
+        for(int i = 0; i < notHeard; i++) {
+            s.add(br.readLine());
+        }
+
+        for(int i = 0; i < notLooked; i++) {
+            String tmp = br.readLine();
+            if(s.contains(tmp)) ls.add(tmp);
+        }
+
+
+        ls.sort(String::compareTo);
+        bw.write(ls.size() + "\n");
+
+        for(int i = 0; i < ls.size(); i++){
+            bw.write(ls.get(i) + "\n");
+        }
+
+        bw.flush();
+    }
+}
