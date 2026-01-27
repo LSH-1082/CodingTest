73b732d1b0652c5ebfda9aab6c025c5d11cadf4c
LSH <65267061+LSH-1082@users.noreply.github.com>
2026-01-27 21:50:43 +0900

    [Silver III] Title: 피보나치 함수, Time: 116 ms, Memory: 16180 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -6,26 +6,24 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int n = Integer.parseInt(br.readLine());
+        int[][] count = new int[41][2];
+        fibo(count);
 
         while(n --> 0) {
             int num = Integer.parseInt(br.readLine());
-            int[][] count = new int[num + 1][2];
-            fibo(num, count);
             bw.write(count[num][0] + " " + count[num][1] + "\n");
         }
 
         bw.flush();
     }
 
-    public static void fibo(int num, int[][] count) {
+    public static void fibo(int[][] count) {
         count[0][0] = 1;
         count[0][1] = 0;
-        if(num == 0) return;
         count[1][0] = 0;
         count[1][1] = 1;
-        if(num == 1) return;
 
-        for(int i = 2; i <= num; i++) {
+        for(int i = 2; i <= 40; i++) {
             count[i][0] = count[i - 1][0] + count[i - 2][0];
             count[i][1] = count[i - 1][1] + count[i - 2][1];
         }
